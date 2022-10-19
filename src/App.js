763b80027ac7e113a3c1e37d9719129e686591ee
nrocpop <tarobinsontoast@gmail.com>
2022-10-19 16:58:29 -0400

    added initial ui components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { ActionCard } from './ui-components';
 
 /* src/App.js */
 const formFields = {
@@ -19,6 +20,9 @@ function App({ signOut, user }) {
 
   return (
     <>
+
+
+
       {/* Add Todo JSX here  */}
       <Heading level={1}>Hello {user.username}</Heading>
       <Button onClick={signOut}>Sign out</Button>
