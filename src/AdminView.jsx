854056db0d85bcd6cfd67e308124a1fe778d549d
Paul <paulanceallen@live.com>
2022-11-06 15:24:05 -0500

    name

---
+++ src/AdminView.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+
+export function AdminNewAcct(){
+
+
+    return (
+        <>
+        <h1>ADMIN newAccount Page</h1>
+        
+        </>
+        
+    )
+    
+
+}
+export function AdminHome(){
+
+
+    return (
+        <>
+        <h1>ADMIN home</h1>
+        
+        </>
+        
+    )
+    
+
+}
+export function AdminViewAcct(){
+
+
+    return (
+        <>
+        <h1>ADMIN view accounts</h1>
+        
+        </>
+        
+    )
+    
+
+}
+export function AdminNewUser(){
+
+
+    return (
+        <>
+        <h1>ADMIN  newUser</h1>
+        
+        </>
+        
+    )
+    
+
+}
