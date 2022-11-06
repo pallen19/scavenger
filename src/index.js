ce549d020e61bf25600a70daf74f5a70ee190ac8
nrocpop <tarobinsontoast@gmail.com>
2022-11-06 00:07:19 -0400

    fixed index

--- src/index.js
+++ src/index.js
@@ -16,18 +16,8 @@ export default function Entrypoint() {
             {/* catch missing routes */}
         </Routes>
         </BrowserRouter>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    )
+}
 
 const element = document.getElementById('root');
 const root = ReactDOM.createRoot(element);
