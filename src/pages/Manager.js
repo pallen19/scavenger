e83316f2ab505c32123bdede7a4ada3c72d3cad2
Paul <paulanceallen@live.com>
2022-11-05 23:57:16 -0400

    new changes

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -9,29 +9,10 @@ import buttonCreateNewUser from '../components/buttonCreateNewUser';
 import buttonThreeFiveTwoTwoOneSixFiveTwoSixOnClick from '../ui-components/AdminViewHome';
 
 export const Manager = () => {
-  //const createuseronclick = useNavigateAction({ type: "url", url: "NewUser" });
-  const [isToggled, setisToggled] = useState(false);
-  //const [goToNewAccount, setGoToNewAccount] = React.useState(false);
-const handleClick =()=>{
-  
-  
-}
-
-  return (
-    <div>
-      
-       {/* <AdminViewHome
-        overrides={{'Button352216526':{ color: 'white' , isDisabled:false },
-        'Button352216544':{onClick: handleClick} , 
-        'Button352216553':{onClick: handleClick}
-                  
-                  }} />
-                {/*  {isToggled ? <AdminViewNewUser/> : <p>the valdue is valse</p>} */}
-                {/*,onClick:setisToggled(!isToggled)*/}
-                 
-          
-
-    </div>
-  );
-}
+  return(
+  <>
+  <ManagerViewHome/>
+    </>
+    )
+  }
 export default Manager;
