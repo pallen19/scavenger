5d34eccc18c7c118c1ac3ad94d6148634fb37cb7
GitWreck3d <93147948+GitWreck3d@users.noreply.github.com>
2022-11-08 17:21:29 -0500

    BROKE SOME THINGS

--- src/AdminView.jsx
+++ src/AdminView.jsx
@@ -1,5 +1,12 @@
+import { Flex } from "@aws-amplify/ui-react"
 import React from "react"
-
+import { AccountCard } from "./ui-components"
+import { useState } from "react";
+import { db, storage } from './firebase'
+import * as firebase from 'firebase/app'
+import { collection, getDocs, getDoc, updateDoc, query, where, arrayUnion, documentId, doc, arrayRemove, addDoc } from 'firebase/firestore'
+import "firebase/firestore"
+import { ref, uploadBytes } from 'firebase/storage'
 
 export function AdminNewAcct(){
 
@@ -27,18 +34,18 @@ export function AdminHome(){
     
 
 }
-export function AdminViewAcct(){
 
+export function AdminViewAcct(){
+    <div class="flex-container"></div>
+    const accountsColRef = collection(db, "accounts");
+    let count = accountsColRef.size;
 
     return (
         <>
         <h1>ADMIN view accounts</h1>
         
-        </>
-        
-    )
-    
-
+        </> 
+     )
 }
 export function AdminNewUser(){
 
