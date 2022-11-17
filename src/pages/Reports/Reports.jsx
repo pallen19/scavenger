d0976ddfcb0b8fc681487237e00a7689a2275775
nrocpop <tarobinsontoast@gmail.com>
2022-11-16 19:59:11 -0500

    added children property to modal, configured tabs

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,3 +1,8 @@
+import {Outlet,useNavigate,useLocation, Routes, Route} from "react-router-dom";
+import {useState} from "react";
+import { PageHeader } from "../../ui-components";
+import { TestA } from "./Subpages/testa";
+
 export function Reports(props){
     //Constants
     const [currentPage,setCurrentPage] = useState("Passwords")
@@ -15,20 +20,30 @@ export function Reports(props){
            PageHeader: {width:"100%"},
            Background:{width: "100%"},
            PageTitle: {children: "Reports"},
-           SubNavigation:{children:""}
+           SubNavigation:{children:""}}}/>
            
-       }}/>
+           <Outlet/>
           </> );
       case 'Managers':
           return(
+          <>
           <h1>Managers</h1>
+          <Outlet/>
+          </>
           )
       case 'Accountant':
           return(
+            <>
               <h1>Accountant</h1>
+              <Outlet/>
+            </>
               )
       default:
-          return(<h1>Access Denied</h1>)
+          return(
+          <>
+          <h1>Access Denied</h1>
+          <Outlet/>
+          </>)
       }
    
   }
