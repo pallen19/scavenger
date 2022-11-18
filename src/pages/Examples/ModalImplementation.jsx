cbffe5e4f58698cc995e4549b94ee0fb879f7210
nrocpop <tarobinsontoast@gmail.com>
2022-11-18 12:03:16 -0500

    aded modal test

--- src/pages/Examples/ModalImplementation.jsx
+++ src/pages/Examples/ModalImplementation.jsx
@@ -3,22 +3,24 @@ import { useState } from "react";
 import Modal from '../../../components/Modal/Modal'
 //Example of how to implement a modal window.
 export function TestA(props){
+    //The showModal state
     const [showModal,setShowModal] = useState(false);
-
+    //Callback function to close the window.
     const onClose=()=>{
         setShowModal(false);
     }
-    const ModalContent=()=>{
-        
-    }
+
+   
     return(
         <div>
+            {/* We need to have some way to open teh modal window. We can use a button
+            that flips the state by calling setShowModal in a function on click */}
             <button onClick={()=>{setShowModal(!showModal)}}></button>
             <Modal show={showModal} onClose={()=>onClose()}>
-                <p>THIS IS THE PAGE CONTENT</p>
+
+                {/*The page content you want to render will be laced here*/}
+
             </Modal>
-            <p>Test A</p>
-            <Outlet/>
         </div>
     )
 }
