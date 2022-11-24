dc62a8d4ac8eb35872a8de09c9475e895d188718
Andreeb27 <dumas.a27@gmail.com>
2022-11-24 10:38:40 -0500

    starting date

--- src/pages/Reports/Subpages/ExpiredPasswords.jsx
+++ src/pages/Reports/Subpages/ExpiredPasswords.jsx
@@ -1,18 +1,19 @@
-import {collection,getDocs,} from 'firebase/firestore';
-import {db} from '../../../firestore-config'
-import {PageHeader, ReportsCard} from '../../../ui-components';
-import {useState,useEffect} from 'react';
+import { collection, getDocs, } from 'firebase/firestore';
+import { db } from '../../../firestore-config'
+import { PageHeader, ReportsCard } from '../../../ui-components';
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 
-export function ExpiredPasswords(props){
+export function ExpiredPasswords(props) {
     //Constants
-   
+
     const [userInfo, setUserInfo] = useState([]);
 
-    const usersColRef = collection(db, "testUsers");
+    const usersColRef = collection(db, "users");
 
-
+    const currDate = new Date() - 7776000;
+    const currDate1 = new Date();
 
     useEffect(() => {
 
@@ -25,13 +26,34 @@ export function ExpiredPasswords(props){
         getUsers()
 
     }, [])
+
+    function getExpiredUsers(userDate) {
+
+    }
+
     //End of Constants
-return(
-    <div>
-        <p>Expired Passwords</p>
-    {/* {userInfo.map(account1 => ReportsCard(account1))} */}
-    </div>
-)
+
     
-   
-  }
+
+    return (
+        <div>
+            <p>Expired Passwords</p>
+
+            {userInfo.map((user) => {
+                return (
+                    ((new Date().getDate() - new Date(user.creationDate).getDate()) > 90   ) ?
+                        <div>
+                            {" "}
+                            <h1>Name: {user.firstName}</h1>
+                            <h2>Date Generated: {user.creationDate}</h2>
+
+                            {/* {getExpiredPasswords(user)} */}
+                        </div>
+                    : console.log(new Date("11-05-2022"))
+                );
+            })}
+        </div>
+    )
+
+
+}
