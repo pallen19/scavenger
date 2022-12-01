217e7c4b0d0d854ff76d8cc698a8bac8e6de1c43
Paul <paulanceallen@live.com>
2022-12-01 00:28:26 -0500

    generalLedger

--- src/pages/Accounts/Subpages/GeneralLedger.jsx
+++ src/pages/Accounts/Subpages/GeneralLedger.jsx
@@ -1,5 +1,8 @@
 import react from 'react';
 import DataTable from 'react-data-table-component';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../../firestore-config'
+import { useState, useEffect } from 'react';
 
 
 const columns = [
@@ -34,20 +37,54 @@ const data = [
 ]
 
 function GeneralLedger(props,props2,props3,props4) {
+    const approvedJournalEntriesColRef = collection(db, "approvedJournalEntries")
+    const [ApprovedJournals, setapprovedJournals] = useState([])
+    
+
+    useEffect(() => {
+
+        const getapprovedJournalEntries = async () => {
+            const data = await getDocs(approvedJournalEntriesColRef); // handle promise, will return all documents from a specific collection
+            setapprovedJournals(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        }
+
+        getapprovedJournalEntries()
+
+    }, [])
+
     return (
         <>
-        <h3>Asset Accounts</h3>
+        {/* <h3>Asset Accounts</h3>
         <DataTable
             columns={columns}
             data={data}
-            // columns={props.columns}
-            // data={props.data}
-        />
-
+             columns={props.columns}
+             data={props.data}
+ */}
+            
+        {/* /> */}
+    {
+    ApprovedJournals.map((eventChange) => {
+                
+                <div>
+                    
+                    <h1>Account Name: {eventChange.accountName}</h1>
+                    <h2>Actions: {eventChange.actions}</h2>
+                    <h3>Credit:  {eventChange.credit}</h3>
+                    <h3>Debit:  {eventChange.debit}</h3>
+                    <h3>Entry Date: {eventChange.entryDate}</h3>
+                </div>
         
+    }
+    )
+    
+        
+    
+}
         
         </>
     );
 };
+
 
 export default GeneralLedger;
