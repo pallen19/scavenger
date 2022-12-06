de1b24937a836b4ede24cd0cb9266cbe834c66fb
Andreeb27 <dumas.a27@gmail.com>
2022-12-06 00:03:53 -0500

    .

--- src/components/Table/TableFunctions.jsx
+++ src/components/Table/TableFunctions.jsx
@@ -116,149 +116,149 @@ export function getDeniedJournals(journal, callback) {
 
 export async function approveJournal(journal) {
 
-    const debitAccountID = "";
-    const debitBalance = "";
+    // const debitAccountID = "";
+    // const debitBalance = "";
 
-    const creditAccountID = "";
-    const creditBalance = "";
+    // const creditAccountID = "";
+    // const creditBalance = "";
 
-    const debitAccount = journal.accountName
-    const creditAccount = journal.accountNameCredit
+    // const debitAccount = journal.accountName
+    // const creditAccount = journal.accountNameCredit
 
     const accountsColRef = collection(db, "accounts")
 
-    //getting debit account ID
-    getDocs(accountsColRef)
-        .then(snapshot => {
-            snapshot.forEach(account => {
+    // //getting debit account ID
+    // getDocs(accountsColRef)
+    //     .then(snapshot => {
+    //         snapshot.forEach(account => {
 
-                if (account.data().accountName === debitAccount) {
-                    debitAccountID = account.id; // setting this variable to the document's ID - 'XsZ0SW2vOIfnosVyvGs3'
-                    debitBalance = account.data().endBalance.split(".")
-                }
+    //             if (account.data().accountName === debitAccount) {
+    //                 debitAccountID = account.id; // setting this variable to the document's ID - 'XsZ0SW2vOIfnosVyvGs3'
+    //                 debitBalance = account.data().endBalance.split(".")
+    //             }
 
-                if (account.data().accountName === creditAccount) {
-                    creditAccountID = account.id;
-                    creditBalance = account.data().endBalance.split(".")
-                }
-            })
-        })
+    //             if (account.data().accountName === creditAccount) {
+    //                 creditAccountID = account.id;
+    //                 creditBalance = account.data().endBalance.split(".")
+    //             }
+    //         })
+    //     })
 
 
-    const debitAccountToBeEdited = doc(db, "accounts", debitAccountID)
-    const creditAccountToBeEdited = doc(db, "accounts", creditAccountID)
+    // const debitAccountToBeEdited = doc(db, "accounts", debitAccountID)
+    // const creditAccountToBeEdited = doc(db, "accounts", creditAccountID)
 
-    const newDebitBalance = 0;
+    // const newDebitBalance = 0;
 
-    if(debitBalance.length === 2) 
-    {
-        //["12", "34"] - debitBalance
+    // if(debitBalance.length === 2) 
+    // {
+    //     //["12", "34"] - debitBalance
 
-        //["100", "45"] - account Balance
+    //     //["100", "45"] - account Balance
 
-        if(journal.debit.split(".").length === 2)
-        {
-            const dollars = parseInt(journal.debit.split(".")[0]) + parseInt(debitBalance.split(".")[0])
-            const cents = parseInt(journal.debit.split(".")[1]) + parseInt(debitBalance.split(".")[1])
+    //     if(journal.debit.split(".").length === 2)
+    //     {
+    //         const dollars = parseInt(journal.debit.split(".")[0]) + parseInt(debitBalance.split(".")[0])
+    //         const cents = parseInt(journal.debit.split(".")[1]) + parseInt(debitBalance.split(".")[1])
 
-            if(cents >= 100)
-            {
-                dollars = dollars + ((cents / 100));
-                cents = cents % 100;
-            }
+    //         if(cents >= 100)
+    //         {
+    //             dollars = dollars + ((cents / 100));
+    //             cents = cents % 100;
+    //         }
 
-            newDebitBalance = dollars + "." + cents;
-        }
-        else
-        {
-            const dollars = parseInt(journal.debit) + parseInt(debitBalance.split(".")[0])
-            const cents = parseInt(debitBalance.split(".")[1])
+    //         newDebitBalance = dollars + "." + cents;
+    //     }
+    //     else
+    //     {
+    //         const dollars = parseInt(journal.debit) + parseInt(debitBalance.split(".")[0])
+    //         const cents = parseInt(debitBalance.split(".")[1])
 
-            newDebitBalance = dollars + "." + cents;
-        }
+    //         newDebitBalance = dollars + "." + cents;
+    //     }
 
-    }
-    //debitBalance.length === 1 ... ["1234"]
-    else 
-    {
-        if(journal.debit.split(".").length === 2)
-        {
-            const dollars = parseInt(journal.debit.split(".")[0]) + parseInt(debitBalance)
-            const cents = parseInt(journal.debit.split(".")[1])
+    // }
+    // //debitBalance.length === 1 ... ["1234"]
+    // else 
+    // {
+    //     if(journal.debit.split(".").length === 2)
+    //     {
+    //         const dollars = parseInt(journal.debit.split(".")[0]) + parseInt(debitBalance)
+    //         const cents = parseInt(journal.debit.split(".")[1])
 
-            newDebitBalance = dollars + "." + cents;
+    //         newDebitBalance = dollars + "." + cents;
 
-        }
-        else
-        {
-            const dollars = parseInt(journal.debit) + parseInt(debitBalance)
+    //     }
+    //     else
+    //     {
+    //         const dollars = parseInt(journal.debit) + parseInt(debitBalance)
             
-            newDebitBalance = dollars + "";
-        }
+    //         newDebitBalance = dollars + "";
+    //     }
 
-    }
-
-
+    // }
 
 
 
 
-    const newCreditBalance = 0;
-
-    // CREDIT CHANGE HERE
-
-    if(creditBalance.length === 2) 
-    {
-        //["12", "34"] - debitBalance
-
-        //["100", "45"] - account Balance
-
-        if(journal.credit.split(".").length === 2)
-        {
-            const dollars = parseInt(journal.credit.split(".")[0]) + parseInt(creditBalance.split(".")[0])
-            const cents = parseInt(journal.credit.split(".")[1]) + parseInt(creditBalance.split(".")[1])
-
-            if(cents >= 100)
-            {
-                dollars = dollars + ((cents / 100));
-                cents = cents % 100;
-            }
-
-            newCreditBalance = dollars + "." + cents;
-        }
-        else
-        {
-            const dollars = parseInt(journal.credit) + parseInt(creditBalance.split(".")[0])
-            const cents = parseInt(creditBalance.split(".")[1])
-
-            newCreditBalance = dollars + "." + cents;
-        }
-
-    }
-    //debitBalance.length === 1 ... ["1234"]
-    else 
-    {
-        if(journal.credit.split(".").length === 2)
-        {
-            const dollars = parseInt(journal.credit.split(".")[0]) + parseInt(creditBalance)
-            const cents = parseInt(journal.credit.split(".")[1])
-
-            newCreditBalance = dollars + "." + cents;
-        }
-        else
-        {
-            const dollars = parseInt(journal.credit) + parseInt(creditBalance)
-            newCreditBalance = dollars + "";
-        }
-
-    }
-
-    const newCreditField = { endBalance : newCreditBalance }
-    const newDebitField = { endBalance : newDebitBalance }
 
 
-    await updateDoc(creditAccountToBeEdited, newCreditField);
-    await updateDoc(debitAccountToBeEdited, newDebitBalance);
+    // const newCreditBalance = 0;
+
+    // // CREDIT CHANGE HERE
+
+    // if(creditBalance.length === 2) 
+    // {
+    //     //["12", "34"] - debitBalance
+
+    //     //["100", "45"] - account Balance
+
+    //     if(journal.credit.split(".").length === 2)
+    //     {
+    //         const dollars = parseInt(journal.credit.split(".")[0]) + parseInt(creditBalance.split(".")[0])
+    //         const cents = parseInt(journal.credit.split(".")[1]) + parseInt(creditBalance.split(".")[1])
+
+    //         if(cents >= 100)
+    //         {
+    //             dollars = dollars + ((cents / 100));
+    //             cents = cents % 100;
+    //         }
+
+    //         newCreditBalance = dollars + "." + cents;
+    //     }
+    //     else
+    //     {
+    //         const dollars = parseInt(journal.credit) + parseInt(creditBalance.split(".")[0])
+    //         const cents = parseInt(creditBalance.split(".")[1])
+
+    //         newCreditBalance = dollars + "." + cents;
+    //     }
+
+    // }
+    // //debitBalance.length === 1 ... ["1234"]
+    // else 
+    // {
+    //     if(journal.credit.split(".").length === 2)
+    //     {
+    //         const dollars = parseInt(journal.credit.split(".")[0]) + parseInt(creditBalance)
+    //         const cents = parseInt(journal.credit.split(".")[1])
+
+    //         newCreditBalance = dollars + "." + cents;
+    //     }
+    //     else
+    //     {
+    //         const dollars = parseInt(journal.credit) + parseInt(creditBalance)
+    //         newCreditBalance = dollars + "";
+    //     }
+
+    // }
+
+    // const newCreditField = { endBalance : newCreditBalance }
+    // const newDebitField = { endBalance : newDebitBalance }
+
+
+    // await updateDoc(creditAccountToBeEdited, newCreditField);
+    // await updateDoc(debitAccountToBeEdited, newDebitBalance);
 
     //create doc to be deleted
     const deleteThis = doc(db, "pendingJournalEntries", journal.id)
